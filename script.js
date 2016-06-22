9f3d6200410aee1904045306034e3e06d5be04ed
jason2020 <jtay20@punahou.edu>
2016-06-22 12:50:18 -1000

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Challenge 1
+console.log("HELLO WORLD");
+*/
+
+/* Challenge 2
+var sum = 0;
+for (var i = 2; i < process.argv.length; i++) {
+	sum += Number(process.argv[i]);
+}
+
+console.log(sum);
+*/
+
+/* Challenge 3
+var fs = require('fs'),
+	buf = fs.readFileSync(process.argv[2]),
+	newlines = buf.toString().split('\n').length - 1;
+
+console.log(newlines);
+*/
+
+var fs = require('fs');
+var newline;
+var file = fs.readFile(process.argv[2], function (err, data) {
+	newlines = data.toString().split('\n').length - 1;
+	console.log(newlines);
+});
+
+
+
