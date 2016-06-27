8e76ff51c373b829e4dfbab6a4fdd6c7d7f4981a
jason2020 <jtay20@punahou.edu>
2016-06-27 13:57:47 -1000

    Double-checked code

--- script.js
+++ script.js
@@ -148,8 +148,14 @@ var net = require('net');
 var port = process.argv[2];
 var server = net.createServer(function(socket){
 	var date = new Date();
-	var data = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + "";
+	var month = date.getMonth() + 1;
+	if (month < 10) {
+		month = "0" + month;
+	}
+	var data = date.getFullYear() + "-" + month + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + "";
+	var emptyString = " ";
 	socket.write(data);
+	socket.write(emptyString);
 	socket.end();
 });
 
